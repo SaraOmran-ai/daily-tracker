84c07b908c8f3141859102f0800fc7b0c8ce5a43
Sara Omran <saraomran0077@gmail.com>
2025-12-10 18:11:20 +0800

    Add activity time breakdown pie chart with totals dict

--- dailytracker.py
+++ dailytracker.py
@@ -4,6 +4,7 @@ namelist=[]
 durlist=[]
 activities=[]
 day=input("enter date: ")
+totals={}
 while done == False:
     activity=input("enter activity:")
     startTime=float(input("enter start time:"))
@@ -18,16 +19,19 @@ while done == False:
     choice=input(("press enter to add another activity, or type q to finish:"))
     if choice.lower() == "q":
         done = True
+        
 for item in activities:
     start=item["started at"]
     end=item["ended at"]
     name=item["name"]
     timeTaken=end-start
-    print(name,": lasted",timeTaken,"hours")
-    namelist.append(name)
-    durlist.append(timeTaken)
-print(namelist)
-print(durlist)
+    if name in totals:
+        totals[name]+=timeTaken
+    else:
+        totals[name]=timeTaken
+namelist=list(totals.keys())
+durlist=list(totals.values())
+
 plt.pie(durlist,labels=namelist,autopct='%1.1f%%')
 plt.title(f"{day} break down")
 plt.show()
